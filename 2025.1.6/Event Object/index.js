2d0af5056eb2cdf52c36d2f3d1eb45dd160c61a7
cha jae won <168298900+Hanna2231@users.noreply.github.com>
2025-01-06 23:58:21 +0900

    Add files via upload

---
+++ 2025.1.6/Event Object/index.js
@@ -0,0 +1,12 @@
+// 이벤트 객체 (Event Object)
+const myInput = document.querySelector('#myInput');
+const myBtn = document.querySelector('#myBtn');
+
+function printEvent(event) {
+  console.log(event);
+	event.target.style.color = 'red';
+}
+
+myInput.addEventListener('keydown', printEvent);
+myBtn.addEventListener('click', printEvent);
+//https://www.codeit.kr/topics/interactive-javascript/lessons/3804
